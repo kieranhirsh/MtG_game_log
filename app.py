81b2aa1a5f09c1ba80dff8655fb51c233ca95ef4
kieranhirsh <kieranhirsh@gmail.com>
2025-01-19 20:53:05 +1100

    adding data and graphs pages, forgot a file

--- app.py
+++ app.py
@@ -11,11 +11,26 @@ app.register_blueprint(api_routes)
 @app.route('/')
 def index():
     """ Landing page for the site """
+
+    return render_template('index.html')
+
+@app.route('/data')
+def data():
+    """ Spreadsheets are displayed here """
     # Load the data we need before passing it to the template
     decks = Deck.all(True)
-    player = Player.all(True)
+    players = Player.all(True)
 
-    return render_template('index.html', decks=decks, player=player)
+    return render_template('data.html', decks=decks, players=players)
+
+@app.route('/graphs')
+def graphs():
+    """ Graphs are displayed here """
+    # Load the data we need before passing it to the template
+    decks = Deck.all(True)
+    players = Player.all(True)
+
+    return render_template('graphs.html', decks=decks, players=players)
 
 # Set debug=True for the server to auto-reload when there are changes
 if __name__ == '__main__':
