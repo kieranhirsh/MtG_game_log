e6aa6bf1ed4387b58ed5426ee84f9378953fa63b
kieranhirsh <kieranhirsh@gmail.com>
2025-02-02 23:19:25 +1100

    typos

--- api/v1/players.py
+++ api/v1/players.py
@@ -1,33 +1,33 @@
 """ objects that handles all default RestFul API actions for Player """
 from api.v1 import api_routes
-from models.player import Player
+from crud.player import Player_crud
 
 @api_routes.route('/players', methods=["POST"])
 def players_post():
     """ posts data for new player then returns the player data """
-    return Player.create()
+    return Player_crud.create()
 
 @api_routes.route('/players', methods=["GET"])
 def players_get():
     """ returns players data """
-    return Player.all()
+    return Player_crud.all()
 
 @api_routes.route('/players/<player_id>', methods=["GET"])
 def players_specific_get(player_id):
     """ returns specific player data """
-    return Player.specific(player_id)
+    return Player_crud.specific(player_id)
 
 @api_routes.route('/players/<player_id>', methods=["PUT"])
 def players_edit(player_id):
     """ updates existing player data using specified id """
-    return Player.update(player_id)
+    return Player_crud.update(player_id)
 
 @api_routes.route('/players/<player_id>/decks', methods=["GET"])
 def get_decks_data(player_id):
     """ returns all of a specific player's decks' data """
-    return Player.get_decks_data(player_id)
+    return Player_crud.get_decks_data(player_id)
 
 @api_routes.route('/players/<player_id>', methods=["DELETE"])
 def players_delete(player_id):
     """ deletes existing player data using specified id """
-    return Player.delete(player_id)
+    return Player_crud.delete(player_id)
