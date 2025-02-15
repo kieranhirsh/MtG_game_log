6ad934a096f09c96bfcc1faf5379b4964e6c8d5d
kieranhirsh <kieranhirsh@gmail.com>
2025-02-15 23:00:34 +1100

    make bar chart look good

--- graphs/bar_charts.py
+++ graphs/bar_charts.py
@@ -5,6 +5,7 @@ from io import BytesIO
 
 def make_bar_chart(x_values, y_values, x_label = "", y_label="", title=""):
     fig, ax = plt.subplots()
+    plt.setp(ax.get_xticklabels(), rotation=45)
     ax.bar(x_values, y_values)
     if x_label:
         ax.set_xlabel(x_label)
@@ -12,6 +13,7 @@ def make_bar_chart(x_values, y_values, x_label = "", y_label="", title=""):
         ax.set_ylabel(y_label)
     if title:
         ax.set_title(title)
+    plt.tight_layout()
 
     tmpfile = BytesIO()
     fig.savefig(tmpfile, format='png')
