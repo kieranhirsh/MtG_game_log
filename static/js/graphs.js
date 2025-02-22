afdd43d40c4f545e6033ef3dabfb513233715f76
kieranhirsh <kieranhirsh@gmail.com>
2025-02-23 00:33:50 +1100

    colour identity menu now properly works

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -19,15 +19,33 @@ function toggleGraphType() {
 
 function changeBarXColumnOptions() {
   let input = document.getElementById("bar_x_data").value;
-  document.getElementById("bar_x_type").setAttribute("list",input.concat("_type"));
+
+  if (input === "colour identity") {
+    document.getElementById("bar_x_type").setAttribute("list","colour_identity_type");
+  }
+  else {
+    document.getElementById("bar_x_type").setAttribute("list",input.concat("_type"));
+  }
 }
 
 function changeBarYColumnOptions() {
   let input = document.getElementById("bar_y_data").value;
-  document.getElementById("bar_y_type").setAttribute("list",input.concat("_type"));
+
+  if (input === "colour identity") {
+    document.getElementById("bar_y_type").setAttribute("list","colour_identity_type");
+  }
+  else {
+    document.getElementById("bar_y_type").setAttribute("list",input.concat("_type"));
+  }
 }
 
 function changePieDivisionsOptions() {
   let input = document.getElementById("pie_data").value;
-  document.getElementById("pie_divisions").setAttribute("list",input.concat("_type"));
+
+  if (input === "colour identity") {
+    document.getElementById("pie_divisions").setAttribute("list","colour_identity_type");
+  }
+  else {
+    document.getElementById("pie_divisions").setAttribute("list",input.concat("_type"));
+  }
 }
