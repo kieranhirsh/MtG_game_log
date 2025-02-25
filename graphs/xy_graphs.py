ad18cefba9af61051f6e9570655d20860272b8b7
kieranhirsh <kieranhirsh@gmail.com>
2025-02-25 18:11:04 +1100

    cosmetic changes

--- graphs/xy_graphs.py
+++ graphs/xy_graphs.py
@@ -3,15 +3,17 @@ import base64
 import matplotlib.pyplot as plt
 from io import BytesIO
 
-def make_xy_graph(display, x_values, y_values, x_label = "", y_label="", title=""):
+def make_xy_graph(display, x_values, y_values, x_label="", y_label="", title=""):
     fig, ax = plt.subplots()
     plt.setp(ax.get_xticklabels(), rotation=45)
+
     if display == "line":
         ax.plot(x_values, y_values)
     elif display == "bar":
         ax.bar(x_values, y_values)
     else:
         raise ValueError("No graph display specified (must be bar or line)")
+
     if x_label:
         ax.set_xlabel(x_label)
     if y_label:
