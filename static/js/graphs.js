c95a994bf01383f89d77d3830c274d8f0ea08d48
kieranhirsh <kieranhirsh@gmail.com>
2025-05-18 22:11:26 +1000

    fixing typos

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,11 +1,11 @@
 function toggleGraphType() {
   let DivMenu = document.getElementById("menu");
-  let DataType = document.getElementById("data_types").value;
+  let GraphType = document.getElementById("graph_types").value;
   let DivBar = document.getElementById("bar");
   let DivPie = document.getElementById("pie");
 
   DivMenu.style.display = "block";
-  switch (DataType) {
+  switch (GraphType) {
     case "bar":
       DivBar.style.display = "block";
       DivPie.style.display = "none";
