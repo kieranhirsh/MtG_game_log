f3ae039dff0e5adcd5700b0d6121561e2ade8e7f
kieranhirsh <kieranhirsh@gmail.com>
2025-05-25 19:10:43 +1000

    commenting code

--- utils/utils.py
+++ utils/utils.py
@@ -2,16 +2,30 @@
 from data import storage
 
 def get_ci_data_from_dropdown_inputs(request_form):
-    ci_raw_list = request_form.getlist("ci_abbr")
+    # initialise varaiables
     ci_abbr_string = ""
+    ci_abbr_list = []
+
+    # get the list of inputs and all colour identity data
+    ci_raw_list = request_form.getlist("ci_abbr")
+    all_ci = storage.get(class_name="Colour_Identity")
+
+    # loop over the raw list of inputs and add its character to a string
     for abbr in ci_raw_list:
         ci_abbr_string += abbr
-    all_ci = storage.get(class_name="Colour_Identity")
-    ci_abbr_list = []
+
+    # loop over all the colour identities and add their colour abbreviations to a list
     for ci in all_ci:
         ci_abbr_list.append(ci.colours)
+
+    # loop over this list of colour abbreviations and compare each item to the input string
     for ci_abbr in ci_abbr_list:
+        # the strings are sorted so that anagrams match
         if sorted(ci_abbr) == sorted(ci_abbr_string):
             desired_ci = ci_abbr
+            break
+
+    # find the desired colour identity object (will be empty is the desired colour identity doesn't exist)
     colour_identity_data = storage.get(class_name="Colour_Identity", key="colours", value=desired_ci)
+
     return colour_identity_data, desired_ci
