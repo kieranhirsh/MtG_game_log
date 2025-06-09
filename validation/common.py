0b2889c06179f10cb024f0bf566e5934ad28d355
kieranhirsh <kieranhirsh@gmail.com>
2025-06-09 20:38:29 +1000

    bug fix: common validation now actually passes both key and value to storage.get()

--- validation/common.py
+++ validation/common.py
@@ -4,7 +4,7 @@ from data import storage
 
 def valid_id(class_name, id):
     # check that the given id exists for the given class
-    if not storage.get(class_name, id):
-        raise ValueError("Invalid %s specified: %s" % (class_name, id))
+    if not storage.get(class_name=class_name, key="id", value=id):
+        raise ValueError("The desired %s could not be found" % (class_name))
 
     return True
