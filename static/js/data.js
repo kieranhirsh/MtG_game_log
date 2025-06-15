3aa5c45cf97fcfb38a0e3b2e17bd875f042270b1
kieranhirsh <kieranhirsh@gmail.com>
2025-06-15 20:26:58 +1000

    improve table sorting functions

--- static/js/data.js
+++ static/js/data.js
@@ -36,19 +36,32 @@ function toggleDataType() {
 }
 
 function sortTable(n) {
-  var table, rows, switching, i, x, y, header, shouldSwitch, dir, switchcount = 0;
+  var table, rows, switching, noswitch, i, x, y, header, shouldSwitch, dir, switchcount = 0;
   table = document.getElementsByClassName("data_table");
   switching = true;
+  noswitch = true;
   header = table[0].rows[0].getElementsByTagName("th")[n].innerHTML;
   // If the row is "Win Rate", we need to remove the trailing % to be able to convert to a int
   if (header === "Win Rate") {
     for (i = 1; i < (table[0].rows.length); i++) {
       x = table[0].rows[i].getElementsByTagName("td")[n];
-      x.innerHTML = x.innerHTML.slice(0,-1);
+      x.innerHTML = x.innerHTML.slice(0, -1);
     }
   }
-  // Set the sorting direction to ascending:
-  dir = "asc";
+  // Set the sorting direction:
+  if (
+    header === "Games Played" ||
+    header === "Win Rate" ||
+    header === "Date" ||
+    header === "Start Time" ||
+    header === "Game Length (Time)" ||
+    header === "Game Length (Turns)" ||
+    header === "Number of Decks"
+  ) {
+    dir = "desc";
+  } else {
+    dir = "asc";
+  }
   /* Make a loop that will continue until
   no switching has been done: */
   while (switching) {
@@ -77,9 +90,8 @@ function sortTable(n) {
           switching = true;
           continue;
         }
-        // Check whether the data is a number of a string
+        // Check whether the data is a number or a string
         if (
-          header === "# Decks" ||
           header === "Games Played" ||
           header === "Win Rate" ||
           header === "Game Length (Turns)" ||
@@ -98,9 +110,8 @@ function sortTable(n) {
           }
         }
       } else if (dir == "desc") {
-        // Check whether the data is a number of a string
+        // Check whether the data is a number or a string
         if (
-          header === "# Decks" ||
           header === "Games Played" ||
           header === "Win Rate" ||
           header === "Game Length (Turns)" ||
@@ -128,11 +139,18 @@ function sortTable(n) {
       // Each time a switch is done, increase this count by 1:
       switchcount++;
     } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
+      /* If no switching has been done, swap the direction and run the while loop again.
+         noswitch is here to ensure we don't have an infinite loop. */
+      if (switchcount == 0 && noswitch) {
+        if (dir == "asc") {
+          dir = "desc";
+          switching = true;
+          noswitch = false;
+        } else if (dir == "desc") {
+          dir = "asc";
+          switching = true;
+          noswitch = false;
+        }
       }
     }
   }
@@ -146,12 +164,18 @@ function sortTable(n) {
 }
 
 function sortColourTables(col) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount;
+  var table, rows, switching, noswitch, header, i, x, y, shouldSwitch, dir, switchcount;
   for (var tab = 1; tab < 5; tab++) {
     table = document.getElementById(tab + "_colours_table");
     switching = true;
+    noswitch = true;
+    header = table.rows[0].getElementsByTagName("th")[col].innerHTML;
     // Initialise some values:
-    dir = "asc";
+    if (header === "# Decks") {
+      dir = "desc";
+    } else {
+      dir = "asc";
+    }
     switchcount = 0;
     /* Make a loop that will continue until
     no switching has been done: */
@@ -181,16 +205,39 @@ function sortColourTables(col) {
             switching = true;
             continue;
           }
+          // Check whether the data is a number or a string
+          if (header === "# Decks") {
+            if (parseInt(x.innerHTML) > parseInt(y.innerHTML) || parseInt(x.innerHTML) == 0) {
+              // If we want to switch, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          } else {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase() || x.innerHTML.toLowerCase() == 0) {
+              // If we want to switch, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          }
           if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase() || x.innerHTML.toLowerCase() == 0) {
             // If so, mark as a switch and break the loop:
             shouldSwitch = true;
             break;
           }
         } else if (dir == "desc") {
-          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-            // If so, mark as a switch and break the loop:
-            shouldSwitch = true;
-            break;
+          // Check whether the data is a number or a string
+          if (header === "# Decks") {
+            if (parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
+              // If so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          } else {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+              // If so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
           }
         }
       }
@@ -202,11 +249,18 @@ function sortColourTables(col) {
         // Each time a switch is done, increase this count by 1:
         switchcount++;
       } else {
-        /* If no switching has been done AND the direction is "asc",
-        set the direction to "desc" and run the while loop again. */
-        if (switchcount == 0 && dir == "asc") {
-          dir = "desc";
-          switching = true;
+        /* If no switching has been done, swap the direction and run the while loop again.
+           noswitch is here to ensure we don't have an infinite loop. */
+        if (switchcount == 0 && noswitch) {
+          if (dir == "asc") {
+            dir = "desc";
+            switching = true;
+            noswitch = false;
+          } else if (dir == "desc") {
+            dir = "asc";
+            switching = true;
+            noswitch = false;
+          }
         }
       }
     }
