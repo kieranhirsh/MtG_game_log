7e0f983969352f83e1f4cc4b23816fa15722f3cc
kieranhirsh <kieranhirsh@gmail.com>
2025-08-28 11:35:57 +1000

    let's hope this fixes it

--- app.py
+++ app.py
@@ -673,7 +673,9 @@ def data_post():
             game_turns = []
 
             if (not deck.last_accessed
-                or ((datetime.now() - deck.last_accessed) > timedelta(hours=5, minutes=59, seconds=59))):
+                or ((datetime.now() - datetime.strptime(str(deck.last_accessed), "%Y-%m-%d %H:%M:%S"))
+                    > timedelta(hours=5, minutes=59, seconds=59))
+               ):
                 try:
                     edhrec_uri = curl_utils.get_edhrec_uri_from_commander_names([deck.commander_name,
                                                                                  deck.partner_name])
